40b5198989f2d19124e87e2981a3a5e142b9b129
Aaron <AaronBatilo@gmail.com>
2017-05-23 19:57:15 -0600

    Added curl script to test post

---
+++ post_insert.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' http://localhost:8080/post-job
+
