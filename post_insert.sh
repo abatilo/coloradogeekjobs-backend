cb3723f4b63772f1e75c6b69b522cb3dcca5fb59
Aaron <AaronBatilo@gmail.com>
2017-06-06 20:00:39 -0600

    Added a line for posting to prod

--- post_insert.sh
+++ post_insert.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' http://localhost:8080/post-job
+#curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' https://coloradogeekjobs-backend.herokuapp.com/post-job
 
