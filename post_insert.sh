ff9dc2c4c63c97ed3a766487c80bbda3051f4b64
Aaron <AaronBatilo@gmail.com>
2017-06-06 20:39:33 -0600

    Sorting in with newest at the top

--- post_insert.sh
+++ post_insert.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' http://localhost:8080/post-job
-#curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' https://coloradogeekjobs-backend.herokuapp.com/post-job
+#curl -H "Content-Type: application/json" -X POST -d '{"city":"Denver","company":"Josh.ai", "description": "Develop device drivers", "email": "jobs@jstar.ai", "how": "Send an email to jobs@jstar.ai", "remote": false, "title": "Software Engineer", "url": "josh.ai"}' http://localhost:8080/post-job
+curl -H "Content-Type: application/json" -X POST -d '{
+  "city":"Denver",
+  "company":"coloradogeekjobs",
+  "description": "Develop device drivers",
+  "email": "jobs@jstar.ai",
+  "how": "Send an email to jobs@jstar.ai",
+  "remote": false,
+  "title": "React/Frontend Engineer",
+  "url": "josh.ai"
+}' https://coloradogeekjobs-backend.herokuapp.com/post-job
 
